04bfbabf30148757f3c4496daae4f02e3aba0c7f
iLexN <ilex.job@gmail.com>
2020-05-23 14:08:36 +0800

    add str to String

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,20 @@ fn main() {
 
 fn usage() {
     println!("Learning rust.");
-
+    println!("{}" , get_title());
     let the_version = env!("CARGO_PKG_VERSION");
     println!("Version: {}", the_version);
+}
+
+fn get_title() -> String {
+    let mut the_title = String::from("");
+    the_title.push_str(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("),");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+
+    the_title
 }
 
 // fn get_version() -> u8 {
