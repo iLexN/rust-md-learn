f5db918676f27009c31b123b32e43bef91a9c315
iLexN <ilex.job@gmail.com>
2020-05-23 14:15:49 +0800

    add more string

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,33 @@ fn main() {
 }
 
 fn usage() {
-    print_short_banner()
+    print_short_banner();
+    print_long_banner();
+}
+
+fn print_long_banner() {
+    println!("{}", get_title());
+    println!("Written by: {}", env!("CARGO_PKG_AUTHORS"));
+    println!("Homepage: {}", env!("CARGO_PKG_HOMEPAGE"));
+    println!("Usage: tinymd <somefile>.md");
+
+    //v2
+    //print_short_banner();
+    // let mut written_by = String::from("Written by: ");
+    // written_by.push_str(env!("CARGO_PKG_AUTHORS"));
+    // let mut homepage = String::from("Homepage: ");
+    // homepage.push_str(env!("CARGO_PKG_HOMEPAGE"));
+    // let mut usage = String::from("Usage: tinymd <somefile>.md");
+    // println!("{}", written_by);
+    // println!("{}", homepage);
+    // println!("{}", usage);
+
+    //v3
+    // print_short_banner();
+    // println!("Written by: {}\nHomepage: {}\nUsage: tinymd <somefile>.md\n",
+    //          env!("CARGO_PKG_AUTHORS"),
+    //          env!("CARGO_PKG_HOMEPAGE")
+    // );
 }
 
 fn print_short_banner() {
