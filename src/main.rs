5109702f4ca53c5680e2eec1bb24b6907cb02ce2
iLexN <ilex.job@gmail.com>
2020-05-23 13:51:22 +0800

    Creating a string variable in Rust

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ fn main() {
 fn usage() {
     println!("Learning rust.");
 
-    let version = get_version();
-    println!("Version: {}", version);
+    let the_version:&str = "0.1";
+    println!("Version: {}", the_version);
 }
 
-fn get_version() -> u8 {
-    let version = 1;
-    if version < 2 {
-        return 1;
-    }
-
-    2
-}
+// fn get_version() -> u8 {
+//     let version = 1;
+//     if version < 2 {
+//         return 1;
+//     }
+//
+//     2
+// }
